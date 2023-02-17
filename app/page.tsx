cc31efa5eb1b366e9cf42489a2b17aa02c350f38
Martin Loetzsch <martin.loetzsch@gmail.com>
2023-02-17 14:50:37 +0100

    simplify layout

--- app/page.tsx
+++ app/page.tsx
@@ -7,20 +7,27 @@ export default function Home() {
     return (
         <main className={styles.main}>
             <Picture image={imageHavelAlm1} alt="Havel Alm Kratzeburg" priority={true} width={50}>
-                <h1 style={{top: '8vw'}}>Havel Alm</h1>
-                <h2 style={{top: '25vw'}}>68 meters above sea level</h2>
+                <div style={{top: '8vw'}}>
+                    <h1>Havel Alm</h1>
+                    <h2>68 meters above sea level</h2>
+                </div>
                 <div style={{top: '95vw'}}>
                     <p>At the source of the Havel river</p>
                     <p>80 minutes by direct train from Berlin Gesundbrunnen</p>
                 </div>
 
-                <p style={{'top': '211vw'}}>
-                    Havel Alm <a href="https://sabine-boettger.de">Sabine Boettger</a> und <a
-                    href="https://martin-loetzsch.de">Martin Loetzsch</a> GbR<br/>
-                    <a href="https://goo.gl/maps/AE6Nh2RedGxnfaSi9">Dorfstr. 2a, 17237 Kratzeburg</a><br/>
-                    <a href="mailto:info@havel-alm.de">info@havel-alm.de</a><br/>
-                    +49 178 6803711
-                </p>
+                <div style={{'top': '207vw'}}>
+                    <p>Havel Alm <a href="https://sabine-boettger.de">Sabine Boettger</a> und <a
+                        href="https://martin-loetzsch.de">Martin Loetzsch</a> GbR
+                    </p>
+                    <p>
+                        <a href="https://goo.gl/maps/AE6Nh2RedGxnfaSi9">Dorfstr. 2a, 17237 Kratzeburg</a>
+                    </p>
+                    <p>
+                        <a href="mailto:info@havel-alm.de">info@havel-alm.de</a>
+                    </p>
+                    <p>+49 178 6803711</p>
+                </div>
             </Picture>
 
         </main>
