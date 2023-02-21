a30eeaaff8e13239d00aca78f283f88032b30b1a
Martin Loetzsch <martin.loetzsch@gmail.com>
2023-02-21 16:00:19 +0100

    add missing files

---
+++ components/photos.tsx
@@ -0,0 +1,9 @@
+
+
+export default class Photo{
+    constructor(public src: string, public alt: string, public width: number, public height: number) {}
+}
+
+
+export const house_views_Havel_Alm_Kratzeburg_aerial_view_1_jpg = new Photo('/photos/house views/Havel Alm Kratzeburg aerial view 1.jpg', 'Havel Alm Kratzeburg aerial view 1', 1107, 2601)
+export const house_views_House_view_from_street_winter_1_jpg = new Photo('/photos/house views/House view from street winter 1.jpg', 'House view from street winter 1', 4624, 2238)
---
+++ parse_images.py
@@ -0,0 +1,23 @@
+import pathlib
+
+from PIL import Image
+
+photos_dir = pathlib.Path('./public/photos')
+
+# output = sys.stdout
+with open('./components/photos.tsx', 'w') as output:
+
+    print('''
+
+export default class Photo{
+    constructor(public src: string, public alt: string, public width: number, public height: number) {}
+}
+
+''', file=output)
+
+    for path in photos_dir.glob('**/*.jpg'):
+        relative_file_name = str(path)[len(str(photos_dir)) + 1:]
+        variable_name = relative_file_name.replace(' ', '_').replace('.', '_').replace('/', "_")
+        with Image.open(path) as image:
+            print(f'''export const {variable_name} = new Photo('/photos/{relative_file_name}', '{path.stem}', {image.width}, {image.height})''',
+                  file=output)
