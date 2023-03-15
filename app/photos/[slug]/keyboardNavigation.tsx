a3fa6aba91db9076f0f8491c7b6447ad7ed234a7
Martin Loetzsch <martin.loetzsch@gmail.com>
2023-03-15 15:38:39 +0100

    minor

--- app/photos/[slug]/keyboardNavigation.tsx
+++ app/photos/[slug]/keyboardNavigation.tsx
@@ -5,6 +5,7 @@ import {useEffect} from "react";
 import useWindowWidth from "@/components/useWindowWidth"
 
 import * as React from "react";
+import {Simulate} from "react-dom/test-utils";
 
 type KeyboardNavigationProps = {
     linkOnLeftArrow?: string ,
@@ -20,23 +21,40 @@ export const KeyboardNavigation: React.FunctionComponent<KeyboardNavigationProps
     const router = useRouter();
     const windowWidth = useWindowWidth()
 
+
     useEffect(() => {
         if (windowWidth) {
             window.scrollTo({top: Math.round(windowWidth * 0.22), behavior: 'smooth'})
         }
+
+        const navigateNext = () => {
+            if (linkOnRightArrow) {
+                console.log('navigateNext', linkOnRightArrow)
+                router.push(linkOnRightArrow, {forceOptimisticNavigation:true})
+            }
+        }
+
+        const navigatePrevious = () => {
+            if (linkOnLeftArrow) {
+                router.push(linkOnLeftArrow)
+            }
+        }
+
         const keyDownHandler = (e:KeyboardEvent):any => {
-            if (e.code == 'ArrowRight' && linkOnRightArrow) {
-                router.push(linkOnRightArrow);
-            } else if (e.code == 'ArrowLeft' && linkOnLeftArrow) {
-                router.push(linkOnLeftArrow);
+            if (e.code == 'ArrowRight') {
+                navigateNext()
+            } else if (e.code == 'ArrowLeft') {
+                navigatePrevious()
             }
 
         }
-        document.addEventListener("keydown", keyDownHandler);
+
+
+        document.addEventListener("keydown", keyDownHandler)
 
         // clean up
         return () => {
-            document.removeEventListener("keydown", keyDownHandler);
+            document.removeEventListener("keydown", keyDownHandler)
         };
     }, [linkOnLeftArrow, linkOnRightArrow, router, windowWidth]);
 
