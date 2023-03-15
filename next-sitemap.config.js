eeaa05e039d8de515656da1ecd96bf01bb321545
Martin Loetzsch <martin.loetzsch@gmail.com>
2023-03-15 13:48:01 +0100

    add sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next-sitemap').IConfig} */
+
+module.exports = {
+  siteUrl: 'https://havel-alm.de',
+  changefreq: 'daily',
+  priority: 0.7,
+  sitemapSize: 20000,
+  generateIndexSitemap: false,
+  generateRobotsTxt: true
+}
