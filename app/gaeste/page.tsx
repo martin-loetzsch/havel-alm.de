2f746a6c8de705ddd70a6efc08772677c0cd66ed
Martin Loetzsch <martin.loetzsch@gmail.com>
2023-05-23 15:12:47 +0200

    start gaeste page

---
+++ app/gaeste/page.tsx
@@ -0,0 +1,41 @@
+import Photo from '@/components/photo/photo'
+import Grid from "@/components/grid/grid"
+import type { Metadata } from 'next';
+import Image from "next/image"
+import photos from '@/components/photos'
+import grundrissEG from '@/public/grundrisse/Grundriss EG.svg'
+import grundrissDG from '@/public/grundrisse/Grundriss DG.svg'
+import TextCard from '@/components/card/textCard';
+import Card from '@/components/card/textCard';
+
+const title = "Gäste"
+const description = "Informationen für Ihren Aufenthalt"
+
+export const metadata: Metadata = {
+    title: title,
+    description: description,
+    authors: [{ name: 'Havel Alm', url: 'https://havel-alm.de' }],
+    openGraph: {
+        title: title,
+        description: description,
+        url: 'https://havel-alm.de/',
+        siteName: 'havel-alm.de',
+        locale: 'de-DE',
+    },
+
+    icons: {
+        icon: '/favicon.ico',
+    }
+}
+
+export default function Home() {
+    return (
+        <>
+            <h1>Willkommen auf der Havel Alm</h1>
+
+
+
+        </>
+    )
+}
+
