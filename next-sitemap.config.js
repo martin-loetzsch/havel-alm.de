3bc94792c47eb19c7cea8c69188459f018262472
Martin Loetzsch <martin.loetzsch@gmail.com>
2023-08-24 13:11:33 +0200

    exclude '/gaeste' from sitemap.xml

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next-sitemap').IConfig} */
-
 module.exports = {
   siteUrl: 'https://havel-alm.de',
   changefreq: 'daily',
+  exclude: ['/gaeste'],
   priority: 0.7,
   sitemapSize: 20000,
   generateIndexSitemap: false,
