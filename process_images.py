6de7711b1e6112b3244e2b0223646722c1aa7500
Martin Loetzsch <martin.loetzsch@gmail.com>
2023-09-19 14:49:52 +0200

    increase preview image sizes

--- process_images.py
+++ process_images.py
@@ -75,7 +75,7 @@ for path in photos_dir.glob('**/*.[jJ][pP][gG]'):
         height = image.height
 
         # create review image as base64 encoded data url
-        image.thumbnail(size=(20, 20))
+        image.thumbnail(size=(30, 30))
         # image.show()
         buffered = io.BytesIO()
         image.save(buffered, format="PNG")
