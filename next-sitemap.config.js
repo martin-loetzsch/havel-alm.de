713f9428143454c392822beeb6e175d963f01b69
Martin Loetzsch <martin.loetzsch@gmail.com>
2023-09-25 14:28:26 +0200

    adapt robots.txt

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,10 +1,24 @@
+let exclude = ['/gaeste', '/making-of']
+
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: 'https://havel-alm.de',
   changefreq: 'daily',
-  exclude: ['/gaeste', '/making-of'],
+  exclude: exclude,
   priority: 0.7,
   sitemapSize: 20000,
   generateIndexSitemap: false,
-  generateRobotsTxt: true
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [,
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+      {
+        userAgent: '*',
+        disallow: exclude,
+      },
+    ],
+  },
 }
