2def3856f4a27e30e12fc1817f8e895a7a3f3511
Martin Loetzsch <martin.loetzsch@gmail.com>
2023-11-07 18:31:45 +0100

    load komoot iframes lazily

--- app/kratzeburg/komootMap.tsx
+++ app/kratzeburg/komootMap.tsx
@@ -7,7 +7,10 @@ interface KomootMapProps {
 const KomootMap: React.FC<KomootMapProps> = ({ tourId }) => {
     return (
         <iframe src={`https://www.komoot.de/tour/${tourId}/embed?share_token=ag7kIsWCZjzLb7ivSzr4xY1Ret9JbtUKIokSZUInQLMQjYywhA`}
-            width="100%" height="100%"></iframe>
+            width="100%" height="100%"
+            loading="lazy">
+
+            </iframe>
     );
 };
 
