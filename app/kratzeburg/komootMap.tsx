a90826c01372047a69952032569cb47e40fc84bb
Martin Loetzsch <martin.loetzsch@gmail.com>
2023-11-07 21:20:13 +0100

    add titles to komoot iframes

--- app/kratzeburg/komootMap.tsx
+++ app/kratzeburg/komootMap.tsx
@@ -7,10 +7,11 @@ interface KomootMapProps {
 const KomootMap: React.FC<KomootMapProps> = ({ tourId }) => {
     return (
         <iframe src={`https://www.komoot.de/tour/${tourId}/embed?share_token=ag7kIsWCZjzLb7ivSzr4xY1Ret9JbtUKIokSZUInQLMQjYywhA`}
-            width="100%" height="100%"
-            loading="lazy">
-
-            </iframe>
+            width="100%"
+            height="100%"
+            loading="lazy"
+            title={`Komoot map ${tourId}`}>
+        </iframe>
     );
 };
 
