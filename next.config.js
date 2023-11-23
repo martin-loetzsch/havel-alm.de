a683c40e92478606238945151303b35bb69e553b
Martin Loetzsch <martin.loetzsch@gmail.com>
2023-11-23 16:39:05 +0100

    cleanup next.config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    // appDir: true,
-    // typedRoutes: true,
+
   },
 }
-
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-});
-module.exports = withBundleAnalyzer(nextConfig);
