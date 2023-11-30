4c276d2dea224864fd893ea176c42bc13e7c2e75
Martin Loetzsch <martin.loetzsch@gmail.com>
2023-11-30 13:23:38 +0100

    improve cookie parsing

--- app/(tracking)/x/route.ts
+++ app/(tracking)/x/route.ts
@@ -3,13 +3,16 @@ import { NextRequest, NextResponse, userAgent } from "next/server"
 
 
 export async function POST(request: NextRequest) {
-    console.log(userAgent(request))
+    const cookies = Object.values(Object.fromEntries(request.cookies))
+    const cookiesFlattened = Object.fromEntries(cookies.map((cookie: any) => [cookie.name, cookie.value]))
+ //   console.log()
+    const body = await request.json()
     const event = {
+        pathname: body.pathname,
+        searchParams: body.searchParams,
+        referrer: body.referrer,
         headers: Object.fromEntries(await request.headers.entries()),
-        body: await request.json(),
-        ip: request.ip || null,
-        geo: request.geo || null, 
-        cookies: Object.fromEntries(request.cookies),
+        cookies: cookiesFlattened,
         userAgent: userAgent(request),
     }
     return NextResponse.json(event)
