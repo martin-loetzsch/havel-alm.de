52bd031a50cd93b5d2beca81d4b2aa08b1deeeba
Martin Loetzsch <martin.loetzsch@gmail.com>
2023-11-30 12:47:35 +0100

    add more request args

--- app/(tracking)/x/route.ts
+++ app/(tracking)/x/route.ts
@@ -1,14 +1,16 @@
 // route name "/x" is chosen over e.g. "/track" to avoid problems with ad blockers
-import { NextRequest, NextResponse } from "next/server"
+import { NextRequest, NextResponse, userAgent } from "next/server"
 
 
 export async function POST(request: NextRequest) {
-    console.log(request.geo)
+    console.log(userAgent(request))
     const event = {
-        // headers: request.headers.get('headers'),
+        headers: Object.fromEntries(await request.headers.entries()),
+        body: await request.json(),
         ip: request.ip || null,
         geo: request.geo || null, 
-        cookies: Object.fromEntries(request.cookies)
+        cookies: Object.fromEntries(request.cookies),
+        userAgent: userAgent(request),
     }
     return NextResponse.json(event)
 }
