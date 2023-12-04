de8db29f67f82282a3326d9dc752bb45dbe47c0b
Martin Loetzsch <martin.loetzsch@gmail.com>
2023-12-04 14:20:40 +0100

    fix casing bug

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,7 +4,7 @@ import type { Config } from 'drizzle-kit';
 import { connection_string } from '@/app/lib/db';
 
 export default {
-	schema: ['./app/(tracking)/PageViewTable.ts'],
+	schema: ['./app/(tracking)/pageViewTable.ts'],
 	driver: 'pg' ,
 	dbCredentials: {
 		connectionString: connection_string,
