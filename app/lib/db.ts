77a80cc633a2ef620ac2778ff0388d828d6fc1e2
Martin Loetzsch <martin.loetzsch@gmail.com>
2023-12-04 14:21:57 +0100

    add sslMode to connection string

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -1,12 +1,16 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { Pool } from 'pg';
 
-export const connection_string = process.env.POSTGRES_URL || ''
-if (!connection_string) {
+const POSTGRES_URL = process.env.POSTGRES_URL || ''
+
+if (!POSTGRES_URL) {
 	throw new Error("Please set the POSTGRES_URL environment variable");
 }
 
-const pool = new Pool({connectionString: connection_string})
+export const connection_string = POSTGRES_URL + (process.env.SKIP_SSL ? "" : "?sslmode=require")
+
+
+const pool = new Pool({connectionString: connection_string })
 
 const db = drizzle(pool, {logger: false})
 
