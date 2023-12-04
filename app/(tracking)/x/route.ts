6e3c0dd916cb10f5305c6bfdff7db3ca0925a6d6
Martin Loetzsch <martin.loetzsch@gmail.com>
2023-12-04 11:52:03 +0100

    insert tracking events into page view table

--- app/(tracking)/x/route.ts
+++ app/(tracking)/x/route.ts
@@ -1,15 +1,16 @@
 // route name "/x" is chosen over e.g. "/track" to avoid problems with ad blockers
 import { NextRequest, NextResponse, userAgent } from "next/server"
-
+import db from "@/app/lib/db"
+import {PageViewTable} from "../pageViewTable"
 
 export async function POST(request: NextRequest) {
     const cookies = Object.values(Object.fromEntries(request.cookies))
     const cookiesFlattened = Object.fromEntries(cookies.map((cookie: any) => [cookie.name, cookie.value]))
- //   console.log()
     const body = await request.json()
+    
     const event = {
         ...body,
-        timestamp: new Date().toISOString(),
+        timestamp: new Date(),
         // headers: Object.fromEntries(await request.headers.entries()),
         cookies: cookiesFlattened,
         userAgent: userAgent(request),
@@ -22,6 +23,9 @@ export async function POST(request: NextRequest) {
             longitude: request.headers.get('x-vercel-ip-longitude'),
         }
     }
-    console.log(event)
+    //console.log(event)
+
+    await db.insert(PageViewTable).values(event)
+
     return NextResponse.json('ok')
 }
