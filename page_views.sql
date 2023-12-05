14106bf9d1ac0c9b02fe8c0cebfc2c0ae32df8b4
Martin Loetzsch <martin.loetzsch@gmail.com>
2023-12-05 15:46:23 +0100

    improve page views query

--- page_views.sql
+++ page_views.sql
@@ -1,13 +1,16 @@
 select
-    visitor_id,
-    hit,
-    timestamp,
-    url ->> 'pathname' as pathname,
-    user_agent ->> 'ua' as user_agent,
-    array[geo ->> 'country', geo ->> 'region' , geo ->> 'city' , geo ->> 'ip'] as geo,
-    array [referrer ->> 'host', referrer ->> 'pathname', referrer ->> 'searchParams'] as referrer
+    --visitor_id,
+    min(timestamp) as timestamp,
+    count(*) as hits,
+    array_agg(url->>'pathname' order by hit asc) as pathnames,
+    min(user_agent ->> 'ua') as user_agent,
+    min(array[geo ->> 'country', geo ->> 'region' , geo ->> 'city' , geo ->> 'ip']) as geo,
+    min(array [referrer ->> 'host', referrer ->> 'pathname', referrer ->> 'searchParams']) as referrer
 from
     page_view
 where
     internal_request = false
     and url ->> 'host' = 'havel-alm.de'
+group by
+    visitor_id, session_id
+order by timestamp asc
